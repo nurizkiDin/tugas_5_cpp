a016b853a0b264c0de8e1259d3d94e4229e02b31
Nurizki <nutrisi1udin@gmail.com>
2022-10-04 06:16:35 +0700

    tugas 5 cpp
    tugas niomic

---
+++ tugas5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	float x, y;
+	cout<<"Inputkan Nilai Yang Ingin Dipangkatkan : ";
+	cin>>x;
+	cout<<"Inputkan Nilai Pangkat : ";
+	cin>>y;
+	cout<<"Hasil Per Pangkatan : "<<pow(x, y);
+}
